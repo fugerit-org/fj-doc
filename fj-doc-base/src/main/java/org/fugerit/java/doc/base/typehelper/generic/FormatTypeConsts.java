6f6f33e833971e1dc556493c8e85f1586d110943
Fugerit <m@fugerit.org>
2019-11-16 22:08:14 +0100

    POI type and format handling

--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/FormatTypeConsts.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/FormatTypeConsts.java
@@ -4,4 +4,10 @@ public class FormatTypeConsts {
 
 	public static final String TYPE_NUMBER = "number";
 	
+	public static final String TYPE_DATE = "date";
+	public static final String FORMAT_DATE_YYYY_MM_DD = "yyyy-MM-dd";
+	public static final String FORMAT_DATE_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd hh:mm:ss";
+	public static final String FORMAT_DATE_ISO = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	public static final String FORMAT_DATE_DEFAULT = FORMAT_DATE_YYYY_MM_DD;	
+	
 }
