c37d260044fd9042d485038f58271768d1c18859
Fugerit <m@fugerit.org>
2019-11-17 15:46:30 +0100

    Added checkpoint to test timing

--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/FormatTypeConsts.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/FormatTypeConsts.java
@@ -1,13 +1,15 @@
 package org.fugerit.java.doc.base.typehelper.generic;
 
+import org.fugerit.java.core.util.format.TimeFormatDefault;
+
 public class FormatTypeConsts {
 
 	public static final String TYPE_NUMBER = "number";
 	
 	public static final String TYPE_DATE = "date";
-	public static final String FORMAT_DATE_YYYY_MM_DD = "yyyy-MM-dd";
-	public static final String FORMAT_DATE_YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd hh:mm:ss";
-	public static final String FORMAT_DATE_ISO = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	public static final String FORMAT_DATE_YYYY_MM_DD = TimeFormatDefault.DATE_YYYY_MM_DD;
+	public static final String FORMAT_DATE_YYYY_MM_DD_HH_MM_SS = TimeFormatDefault.TIMESTAMP_YYYY_MM_DD_HH_MM_SS;
+	public static final String FORMAT_DATE_ISO = TimeFormatDefault.TIMESTAMP_ISO_TZ;
 	public static final String FORMAT_DATE_DEFAULT = FORMAT_DATE_YYYY_MM_DD;	
 	
 }
