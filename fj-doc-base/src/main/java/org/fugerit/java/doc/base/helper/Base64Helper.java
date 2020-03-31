0c9fbc8a03e8a5e5a0cf691f859a72441cea98ad
Fugerit <m@fugerit.org>
2020-03-31 23:36:18 +0200

    Changed Base64 handler

--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/helper/Base64Helper.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/helper/Base64Helper.java
@@ -1,15 +1,15 @@
 package org.fugerit.java.doc.base.helper;
 
-import org.apache.commons.codec.binary.Base64;
+import java.util.Base64;
 
 public class Base64Helper {
 
 	public static String encodeBase64String( byte[] data ) {
-		return Base64.encodeBase64String( data );
+		return Base64.getEncoder().encodeToString( data );
 	}
 	
 	public static byte[] decodeBase64String( String data ) {
-		return Base64.decodeBase64( data );
+		return Base64.getDecoder().decode( data );
 	}
 	
 }
