c91dfc821462f6c13650ef7d90495842f72c9d00
Matteo Franci <m@fugerit.org>
2022-12-02 17:40:23 +0100

    Changed base path of freemarker config to avoid conflict

--- fj-doc-freemarker/src/main/java/org/fugerit/java/doc/freemarker/helper/FreeMarkerDocProcess.java
+++ fj-doc-freemarker/src/main/java/org/fugerit/java/doc/freemarker/helper/FreeMarkerDocProcess.java
@@ -11,7 +11,7 @@ public class FreeMarkerDocProcess implements Serializable {
 	 */
 	private static final long serialVersionUID = 1277168639030295400L;
 
-	public static final String CONFIG_PATH = "cl://fm_doc/fm-process-config.xml";
+	public static final String CONFIG_PATH = "cl://fj_doc_freemarker_config/fm-process-config.xml";
 	
 	private static DocProcessConfig INSTANCE = DocProcessConfig.loadConfigSafe( CONFIG_PATH );
 
--- fj-doc-mod-fop/src/main/java/org/fugerit/java/doc/mod/fop/FreeMarkerFopProcess.java
+++ fj-doc-mod-fop/src/main/java/org/fugerit/java/doc/mod/fop/FreeMarkerFopProcess.java
@@ -11,7 +11,7 @@ public class FreeMarkerFopProcess implements Serializable {
 	 */
 	private static final long serialVersionUID = 1277168639030295400L;
 
-	public static final String CONFIG_PATH = "cl://fm_fop/fop-process-config.xml";
+	public static final String CONFIG_PATH = "cl://fj_doc_mod_fop_config/fop-process-config.xml";
 	
 	private static DocProcessConfig INSTANCE = DocProcessConfig.loadConfigSafe( CONFIG_PATH );
 
