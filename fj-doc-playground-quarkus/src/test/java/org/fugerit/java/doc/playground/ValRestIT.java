e5a9ef25cf5e370d56865389d4bdafe38203de1a
Matteo Franci <m@fugerit.org>
2022-12-29 17:07:43 +0100

    Fixed playground tests

---
+++ fj-doc-playground-quarkus/src/test/java/org/fugerit/java/doc/playground/ValRestIT.java
@@ -0,0 +1,8 @@
+package org.fugerit.java.doc.playground;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ValRestIT extends ValRestTest {
+    // Execute the same tests but in packaged mode.
+}
--- fj-doc-playground-quarkus/src/test/java/org/fugerit/java/doc/playground/ValRestTest.java
+++ fj-doc-playground-quarkus/src/test/java/org/fugerit/java/doc/playground/ValRestTest.java
@@ -1,4 +1,4 @@
-package org.fugerit.java;
+package org.fugerit.java.doc.playground;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -6,16 +6,18 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.fugerit.java.TestConsts;
+
 @QuarkusTest
-public class GreetingResourceTest {
+public class ValRestTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get( TestConsts.BASE_API_PATH+"/hello" )
+          .when().get( TestConsts.BASE_API_PATH+"/val/supported_extensions" )
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("[\"JPG\",\"TIF\",\"DOCX\",\"XLSX\",\"TIFF\",\"PDF\",\"PNG\",\"DOC\",\"JPEG\",\"XLS\"]"));
     }
 
 }
