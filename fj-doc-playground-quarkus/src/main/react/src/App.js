e07badd520abe3083429fd6ba8e07d78056fc890
Matteo Franci <m@fugerit.org>
2022-12-30 15:41:54 +0100

    Better react interface

--- fj-doc-playground-quarkus/src/main/react/src/App.js
+++ fj-doc-playground-quarkus/src/main/react/src/App.js
@@ -6,10 +6,7 @@ import DevValTestForm from './playground/DocValTestForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      	{/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <DevValTestForm/>
-      </header>
+		 <DevValTestForm/>
     </div>
   );
 }
--- fj-doc-playground-quarkus/src/main/react/src/playground/DocValTestForm.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/playground/DocValTestForm.jsx
@@ -1,73 +1,94 @@
-import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { Component, Fragment } from 'react';
+import { Form, Button, ListGroup } from 'react-bootstrap';
 import appService from '../common/app-service';
 
 class DocValTestForm extends Component {
-	
+
 	constructor(props) {
 		super(props);
 		this.sendForValidation = this.sendForValidation.bind(this);
 		this.prepareSend = this.prepareSend.bind(this);
-      	this.state = {
-            fileToValidate: null,
-            validationResult: null
-        }		
-	}	
+		this.state = {
+			fileToValidate: null,
+			supportedExtensions: null,
+			validationResult: null
+		}
+	}
 
-    sendForValidation = (e) => {
-        e.preventDefault();
-        if ( this.state.fileToValidate == null ) {
-			alert( 'File non provided' );	
+	componentDidMount() {
+		var reactState = this;
+		appService.doAjaxMultipart('GET', '/val/supported_extensions', null).then(response => {
+			if (response.success) {
+				reactState.setState({
+					supportedExtensions: response.result
+				})
+			} else {
+				reactState.setState({
+					supportedExtensions: null
+				})
+			}
+		})
+	}
+
+	sendForValidation = (e) => {
+		e.preventDefault();
+		if (this.state.fileToValidate == null) {
+			alert('File non provided');
 		} else {
 			let formData = new FormData();
 			formData.append('file', this.state.fileToValidate);
-			
+
 			var reactState = this;
 			appService.doAjaxMultipart('POST', '/val/check', formData).then(response => {
-				console.log( response );
-     		 if (response.success) {
-       			 reactState.setState({
-					validationResult: response.result.message
-				})	
-     		 } else {
-				if ( response.result != null && response.result.message != null ) {
-	       			 reactState.setState({
+				if (response.success) {
+					reactState.setState({
 						validationResult: response.result.message
-					})	
+					})
 				} else {
-	       			 reactState.setState({
-						validationResult: response.status
-					})	
+					if (response.result != null && response.result.message != null) {
+						reactState.setState({
+							validationResult: response.result.message
+						})
+					} else {
+						reactState.setState({
+							validationResult: response.status
+						})
+					}
 				}
-      		 }
-    		})
+			})
 		}
-    };	
-    
-    prepareSend = (e) => {
-        e.preventDefault();
-        this.setState({
-			fileToValidate:e.target.files[0]
+	};
+
+	prepareSend = (e) => {
+		e.preventDefault();
+		this.setState({
+			fileToValidate: e.target.files[0]
 		})
-    };	    
-	
-    render() { 
-		let message = '';
-		if ( this.state.validationResult != null ) {
-			message = 'Validation result : '+this.state.validationResult;
+	};
+
+	render() {
+		let message = <Fragment></Fragment>
+		if (this.state.validationResult != null) {
+			message = <b>Validation result : {this.state.validationResult} </b>
 		}
+		let supportedExtensions = <Fragment></Fragment>;
+		if (this.state.supportedExtensions != null) {
+			supportedExtensions = <ListGroup variant="secondary">Supported extensions : {this.state.supportedExtensions.map(d => <ListGroup.Item key={d}>{d}</ListGroup.Item>)}</ListGroup>
+		}
+
 		return (
 			<div>
-			  {message}
-		      <Form.Group controlId="For" className="mb-3">
-		        <Form.Label>Doc Type File validation example: </Form.Label>
-		        <Form.Control type="file" onChange={this.prepareSend}/>  		
-		      </Form.Group>
-		      <Button variant="primary" onClick={this.sendForValidation}>Send</Button>
-		    </div>
-		  );  		
-	}	
-	  
+				{message}
+				<Form.Group controlId="For" className="mb-3">
+					<Form.Label>Doc Type File validation example: </Form.Label>
+					<Form.Control type="file" onChange={this.prepareSend} />
+				</Form.Group>
+				<Button variant="primary" onClick={this.sendForValidation}>Send</Button>
+				{supportedExtensions}
+			</div>
+		);
+	}
+
 }
 
 export default DocValTestForm;
