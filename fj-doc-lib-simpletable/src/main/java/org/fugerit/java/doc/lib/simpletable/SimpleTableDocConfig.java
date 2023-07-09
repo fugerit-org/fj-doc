73df8be11dd407cf0048e590e7ee52896a41c31d
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-07-09 21:12:51 +0200

    SimpleTableDocConfig config review #38

--- fj-doc-lib-simpletable/src/main/java/org/fugerit/java/doc/lib/simpletable/SimpleTableDocConfig.java
+++ fj-doc-lib-simpletable/src/main/java/org/fugerit/java/doc/lib/simpletable/SimpleTableDocConfig.java
@@ -1,11 +1,8 @@
 package org.fugerit.java.doc.lib.simpletable;
 
-import java.io.InputStream;
 import java.io.OutputStream;
 
 import org.fugerit.java.core.cfg.ConfigException;
-import org.fugerit.java.core.lang.helpers.ClassHelper;
-import org.fugerit.java.core.util.filterchain.MiniFilterChain;
 import org.fugerit.java.doc.base.config.DocException;
 import org.fugerit.java.doc.base.config.DocInput;
 import org.fugerit.java.doc.base.config.DocOutput;
@@ -15,40 +12,35 @@ import org.fugerit.java.doc.base.model.DocBase;
 import org.fugerit.java.doc.base.process.DocProcessConfig;
 import org.fugerit.java.doc.base.process.DocProcessContext;
 import org.fugerit.java.doc.base.process.DocProcessData;
+import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfig;
+import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfigFacade;
 import org.fugerit.java.doc.lib.simpletable.model.SimpleTable;
 
 public class SimpleTableDocConfig {
 
-	private final static String CONFIG_PATH = "fj_doc_lib_simpletable/doc-process-config.xml";
+	private static final String CONFIG_ID = "FJ_DOC_LIB_SIMPLETABLE";
 	
-	private DocProcessConfig config;
+	private FreemarkerDocProcessConfig config;
 	
-	private SimpleTableDocConfig( DocProcessConfig config ) {
+	private SimpleTableDocConfig( FreemarkerDocProcessConfig config ) {
 		this.config = config;
 	}
 	
 	public static SimpleTableDocConfig newConfig() throws ConfigException {
-		SimpleTableDocConfig simpleTableDocConfig = null;
-		try ( InputStream is = ClassHelper.loadFromDefaultClassLoader(CONFIG_PATH) )  {
-			DocProcessConfig config = DocProcessConfig.loadConfig( is );
-			simpleTableDocConfig = new SimpleTableDocConfig( config );
-		} catch (Exception e) {
-			throw new ConfigException( "Error creating configuration : "+e, e );
-		}
-		return simpleTableDocConfig;
+		FreemarkerDocProcessConfig config = FreemarkerDocProcessConfigFacade.newSimpleConfig( CONFIG_ID, "/fj_doc_lib_simpletable/template/" );
+		return new SimpleTableDocConfig(config);
 	}
 
 	public static final String CHAIN_ID_SIMPLE_TABLE = "simple_table";
 	
 	public DocProcessConfig getConfig() {
-		return config;
+		return this.config;
 	}
 	
 	private void process( String chainId, DocProcessContext context, DocProcessData data ) throws Exception {
-		MiniFilterChain chain = this.config.getChainCache( chainId );
-		chain.apply( context , data );
+		this.config.process(CONFIG_ID, CHAIN_ID_SIMPLE_TABLE, context, data);
 	}
-	
+		
 	public void processSimpleTable(  SimpleTable simpleTableModel, DocTypeHandler handler, OutputStream os ) throws DocException {
 		try {
 			DocProcessData data = new DocProcessData();
@@ -62,5 +54,5 @@ public class SimpleTableDocConfig {
 			throw new DocException( "Simple table generation error : "+e, e );
 		}
 	}
-	
+
 }
