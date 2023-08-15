a0fb9eae49740823f65fd20f0d8c0a0dafd4167c
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-15 16:25:19 +0200

    Improved javadoc

--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/GenericConsts.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/GenericConsts.java
@@ -1,20 +1,65 @@
 package org.fugerit.java.doc.base.typehelper.generic;
 
+import org.fugerit.java.doc.base.model.DocInfo;
+
+/**
+ * <p>Constants catalog for properties shared by most DocTypeHandler.</p>
+ * 
+ * <p>Basic a listing of "keys" in : &lt;info name="${key}"&gt;${value}&lt;/info&gt;</p>
+ * 
+ * <p>See {@link DocInfo} for document model object.</p>
+ */
 public class GenericConsts {
 
-	// properties for document meta informations
-	public static final String INFO_KEY_DOC_TITLE = "doc-title";
-	public static final String INFO_KEY_DOC_AUTHOR = "doc-author";
-	public static final String INFO_KEY_DOC_SUBJECT = "doc-subject";
-	public static final String INFO_KEY_DOC_VERSION = "doc-version";
-	public static final String INFO_KEY_DOC_CREATOR = "doc-creator";
-	public static final String INFO_KEY_DOC_LANGUAGE = "doc-language";
+	// Generic properties for all (or most) documents - START
 	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-title">See 'doc-title' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_TITLE = "doc-title";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-author">See 'doc-author' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_AUTHOR = "doc-author";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-subject">See 'doc-subject' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_SUBJECT = "doc-subject";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-version">See 'doc-version' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_VERSION = "doc-version";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-creator">See 'doc-creator' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_CREATOR = "doc-creator";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#doc-language">See 'doc-language' documentation</a>
+	 */
+	public static final String INFO_KEY_DOC_LANGUAGE = "doc-language";
+
+	// Generic properties for all (or most) documents - END
+	
+	
+	// Generic properties for fixed size documents (like PDF) - START
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#margins">See 'margins' documentation</a>
+	 */
 	public static final String INFO_KEY_MARGINS = "margins";
+	
 	public static final int POSITION_MARGIN_LEFT = 0;
 	public static final int POSITION_MARGIN_RIGHT = 1;
 	public static final int POSITION_MARGIN_TOP = 2;
 	public static final int POSITION_MARGIN_BOTTOM = 3;
+	
+	// Generic properties for fixed size documents (like PDF) - END
+	
 	
 	public static final String INFO_DEFAULT_TABLE_PADDING = "default-table-padding";
 	public static final String INFO_DEFAULT_TABLE_PADDING_DEF = "0";
--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/html/HtmlHelperConsts.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/html/HtmlHelperConsts.java
@@ -1,7 +1,30 @@
 package org.fugerit.java.doc.base.typehelper.html;
 
+import org.fugerit.java.doc.base.model.DocInfo;
+
+/**
+ * <p>Constants catalog for properties shared by HTML DocTypeHandler.</p>
+ * 
+ * <p>Basic a listing of "keys" in : &lt;info name="${key}"&gt;${value}&lt;/info&gt;</p>
+ * 
+ * <p>See {@link DocInfo} for document model object.</p>
+ */
 public class HtmlHelperConsts {
 
-	public final static String PROP_HTML_ADD_TO_HEAD = "html-add-to-head";
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#html-add-to-head">See 'html-add-to-head' documentation</a>
+	 */
+	public final static String INFO_KEY_HTML_ADD_TO_HEAD = "html-add-to-head";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#html-css-link">See 'html-css-link' documentation</a>
+	 */
+	public final static String INFO_KEY_HTML_CSS_LINK = "html-css-link";
+	
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#html-css-style">See 'html-css-style' documentation</a>
+	 */
+	public final static String INFO_KEY_HTML_CSS_STYLE = "html-css-style";
+	
 	
 }
