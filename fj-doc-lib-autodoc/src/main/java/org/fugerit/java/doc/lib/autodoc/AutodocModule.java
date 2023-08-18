19efa2005e5c65a5a497e01744c0fd31b7d31bbf
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-18 14:13:17 +0200

    Hidden implicit constructor #51

--- fj-doc-lib-autodoc/src/main/java/org/fugerit/java/doc/lib/autodoc/AutodocModule.java
+++ fj-doc-lib-autodoc/src/main/java/org/fugerit/java/doc/lib/autodoc/AutodocModule.java
@@ -2,6 +2,8 @@ package org.fugerit.java.doc.lib.autodoc;
 
 public class AutodocModule {
 
+	private AutodocModule() {}
+	
 	public static final String DEFAULT_HTML_CSS_LINK = "https://venusguides.fugerit.org/src/css/default_venus_docs_style.css";
 	
 }
