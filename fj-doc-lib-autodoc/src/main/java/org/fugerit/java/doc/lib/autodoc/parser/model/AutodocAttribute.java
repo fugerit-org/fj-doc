19d7ed87396f4d2cacbd69f14210b14b47ca4696
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-18 23:01:31 +0200

    Fix issues for sonar cloud scan #51

--- fj-doc-lib-autodoc/src/main/java/org/fugerit/java/doc/lib/autodoc/parser/model/AutodocAttribute.java
+++ fj-doc-lib-autodoc/src/main/java/org/fugerit/java/doc/lib/autodoc/parser/model/AutodocAttribute.java
@@ -15,6 +15,8 @@ public class AutodocAttribute {
 
 	private AutodocModel autodocModel;
 	
+	private static final String EMPTY = "";
+	
 	public AutodocAttribute(XsdAttribute xsdAttribute, AutodocModel autodocModel) {
 		super();
 		this.xsdAttribute = xsdAttribute;
@@ -28,9 +30,9 @@ public class AutodocAttribute {
 	private String parseBaseType( String type ) {
 		String baseType = type;
 		if ( type.startsWith( this.autodocModel.getXsdPrefix() ) ) {
-			baseType = type.replace( this.autodocModel.getXsdPrefix() , "" );
+			baseType = type.replace( this.autodocModel.getXsdPrefix() , EMPTY );
 		} else if ( type.startsWith( this.autodocModel.getAutodocPrefix() ) ) {
-			baseType = type.replace( this.autodocModel.getAutodocPrefix() , "" );
+			baseType = type.replace( this.autodocModel.getAutodocPrefix() , EMPTY );
 		}
 		return baseType;
 	}
@@ -47,6 +49,62 @@ public class AutodocAttribute {
 		return this.getXsdAttribute().getRawName();
 	}
 	
+	private void handleBaseRestriction( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		if ( xsdRestriction.getBase() != null ) {
+			builder.append( " , base : " );
+			builder.append( this.parseBaseType( xsdRestriction.getBase() ) );
+		}
+	}
+	
+	private void handleLenthRestrictions( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		if ( xsdRestriction.getMinLength() != null ) {
+			builder.append( " , minLength : " );
+			builder.append( xsdRestriction.getMinLength().getValue() );	
+		}
+		if ( xsdRestriction.getMaxLength() != null ) {
+			builder.append( " , maxLength : " );
+			builder.append( xsdRestriction.getMaxLength().getValue() );	
+		}
+	}
+	
+	private void handleMinRestrictions( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		if ( xsdRestriction.getMinInclusive() != null ) {
+			builder.append( " , minInclusive : " );
+			builder.append( xsdRestriction.getMinInclusive().getValue() );	
+		}
+		if ( xsdRestriction.getMaxInclusive() != null ) {
+			builder.append( " , maxInclusive : " );
+			builder.append( xsdRestriction.getMaxInclusive().getValue() );	
+		}
+	}
+	
+	private void handleMaxRestrictions( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		if ( xsdRestriction.getMinExclusive() != null ) {
+			builder.append( " , minExclusive : " );
+			builder.append( xsdRestriction.getMinExclusive().getValue() );	
+		}
+		if ( xsdRestriction.getMaxExclusive() != null ) {
+			builder.append( " , maxExclusive : " );
+			builder.append( xsdRestriction.getMaxExclusive().getValue() );	
+		}
+	}
+	
+	private void handlePatternRestriction( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		if ( xsdRestriction.getPattern() != null ) {
+			builder.append( " , pattern : " );
+			builder.append( xsdRestriction.getPattern().getValue() );
+		}
+	}
+	
+	private void handleEnumerationRestriction( StringBuilder builder, XsdRestriction xsdRestriction ) {
+		List<XsdEnumeration> enumeratios = xsdRestriction.getEnumeration();
+		if ( enumeratios != null && !enumeratios.isEmpty() ) {
+			builder.append( " , enumeration : [ " );
+			builder.append( StringUtils.concat( " , " , enumeratios.stream().map( current -> { return current.getValue(); } ).collect( Collectors.toList() ) ) );
+			builder.append( " ]" );
+		}
+	}
+
 	public String getNote() {
 		StringBuilder builder = new StringBuilder();
 		String type = this.parseBaseType( this.getXsdAttribute().getType() );
@@ -54,44 +112,12 @@ public class AutodocAttribute {
 		List<XsdRestriction> restrictions = this.getXsdAttribute().getAllRestrictions();
 		if ( restrictions != null && !restrictions.isEmpty() ) {
 			for ( XsdRestriction xsdRestriction : restrictions ) {
-				if ( xsdRestriction.getBase() != null ) {
-					builder.append( " , base : " );
-					builder.append( this.parseBaseType( xsdRestriction.getBase() ) );
-				}
-				if ( xsdRestriction.getMinLength() != null ) {
-					builder.append( " , minLength : " );
-					builder.append( xsdRestriction.getMinLength().getValue() );	
-				}
-				if ( xsdRestriction.getMaxLength() != null ) {
-					builder.append( " , maxLength : " );
-					builder.append( xsdRestriction.getMaxLength().getValue() );	
-				}
-				if ( xsdRestriction.getMinInclusive() != null ) {
-					builder.append( " , minInclusive : " );
-					builder.append( xsdRestriction.getMinInclusive().getValue() );	
-				}
-				if ( xsdRestriction.getMaxInclusive() != null ) {
-					builder.append( " , maxInclusive : " );
-					builder.append( xsdRestriction.getMaxInclusive().getValue() );	
-				}
-				if ( xsdRestriction.getMinExclusive() != null ) {
-					builder.append( " , minExclusive : " );
-					builder.append( xsdRestriction.getMinExclusive().getValue() );	
-				}
-				if ( xsdRestriction.getMaxExclusive() != null ) {
-					builder.append( " , maxExclusive : " );
-					builder.append( xsdRestriction.getMaxExclusive().getValue() );	
-				}
-				if ( xsdRestriction.getPattern() != null ) {
-					builder.append( " , pattern : " );
-					builder.append( xsdRestriction.getPattern().getValue() );
-				}
-				List<XsdEnumeration> enumeratios = xsdRestriction.getEnumeration();
-				if ( enumeratios != null && !enumeratios.isEmpty() ) {
-					builder.append( " , enumeration : [ " );
-					builder.append( StringUtils.concat( " , " , enumeratios.stream().map( current -> { return current.getValue(); } ).collect( Collectors.toList() ) ) );
-					builder.append( " ]" );
-				}
+				this.handleBaseRestriction(builder, xsdRestriction);
+				this.handleLenthRestrictions(builder, xsdRestriction);
+				this.handleMinRestrictions(builder, xsdRestriction);
+				this.handleMaxRestrictions(builder, xsdRestriction);
+				this.handlePatternRestriction(builder, xsdRestriction);
+				this.handleEnumerationRestriction(builder, xsdRestriction);
 			}
 		}
 		return builder.toString();
