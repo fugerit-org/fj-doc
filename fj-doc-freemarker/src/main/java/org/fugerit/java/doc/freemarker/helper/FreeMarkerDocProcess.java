f97e5a026da5cb768b1234444434c35ba733aec7
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-20 13:51:48 +0200

    Fix issues for sonar cloud scan #51

--- fj-doc-freemarker/src/main/java/org/fugerit/java/doc/freemarker/helper/FreeMarkerDocProcess.java
+++ fj-doc-freemarker/src/main/java/org/fugerit/java/doc/freemarker/helper/FreeMarkerDocProcess.java
@@ -10,14 +10,24 @@ public class FreeMarkerDocProcess implements Serializable {
 	private FreeMarkerDocProcess() {}
 	
 	/**
+	 * Default configuration path
+	 */
+	public static final String CONFIG_PATH = "cl://fj_doc_freemarker_config/fm-freemarker-doc-process-config.xml";
+	
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1277168639030295400L;
 
-	public static final String CONFIG_PATH = "cl://fj_doc_freemarker_config/fm-freemarker-doc-process-config.xml";
-	
-	private static final FreemarkerDocProcessConfig INSTANCE = FreemarkerDocProcessConfigFacade.loadConfigSafe( CONFIG_PATH );
+	/*
+	 * Default implemnentation
+	 */
+	private static final FreemarkerDocProcessConfig INSTANCE = newInstance( CONFIG_PATH );
 
+	public static FreemarkerDocProcessConfig newInstance( String configPath ) {
+		return FreemarkerDocProcessConfigFacade.loadConfigSafe( configPath );
+	}
+	
 	public static FreemarkerDocProcessConfig getInstance() {
 		return INSTANCE;
 	}
