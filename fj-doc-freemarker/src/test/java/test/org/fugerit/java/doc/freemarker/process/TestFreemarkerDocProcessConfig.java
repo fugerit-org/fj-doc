a8fbaf640798b2c4623d8ab1d2a074715bb649f6
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-09-05 10:37:42 +0200

    Added test coverage

--- fj-doc-freemarker/src/test/java/test/org/fugerit/java/doc/freemarker/process/TestFreemarkerDocProcessConfig.java
+++ fj-doc-freemarker/src/test/java/test/org/fugerit/java/doc/freemarker/process/TestFreemarkerDocProcessConfig.java
@@ -6,15 +6,18 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import org.fugerit.java.core.lang.helpers.ClassHelper;
-import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfigValidator;
+import org.fugerit.java.doc.freemarker.config.FreeMarkerConfigStep;
 import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfig;
 import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfigFacade;
+import org.fugerit.java.doc.freemarker.process.FreemarkerDocProcessConfigValidator;
+import org.junit.Assert;
 import org.junit.Test;
 
 import lombok.extern.slf4j.Slf4j;
+import test.org.fugerit.java.BasicTest;
 
 @Slf4j
-public class TestFreemarkerDocProcessConfig {
+public class TestFreemarkerDocProcessConfig extends BasicTest {
 
 	@Test
 	public void testConfigRead001() {
@@ -36,6 +39,24 @@ public class TestFreemarkerDocProcessConfig {
 			String message = "Error : "+e;
 			log.error( message, e );
 			fail(message);
+		}
+	}
+	
+	@Test
+	public void testNewSimpleConfig() {
+		try {
+			Assert.assertNotNull( FreemarkerDocProcessConfigFacade.newSimpleConfig( "simple-config-001",  "/template").getChainCache( "test" ) );
+		} catch (Exception e) {
+			this.failEx(e);
+		}
+	}
+	
+	@Test
+	public void testNewSimpleConfigVersion() {
+		try {
+			Assert.assertNotNull( FreemarkerDocProcessConfigFacade.newSimpleConfig( "simple-config-002",  "/template", FreeMarkerConfigStep.ATT_FREEMARKER_CONFIG_KEY_VERSION_2_3_31 ).getChainCache( "test" ) );
+		} catch (Exception e) {
+			this.failEx(e);
 		}
 	}
 	
