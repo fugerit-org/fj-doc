3a5f710ab24aba25cc95629543f68fa91c1df0de
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-10-16 09:08:37 +0200

    Home page spacing

--- fj-doc-playground-quarkus/src/main/react/src/playground/Home.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/playground/Home.jsx
@@ -61,18 +61,18 @@ class Home extends Component {
 					</Link>
 				</ListItem>
 			</List>
- 			<Grid id="firstRow" container spacing={1}>
+ 			<Grid id="firstRow" container spacing={1} style={{ paddingLeft: 15, paddingTop: 15 }}>
  				<Grid item sx={8}>
  					<p>To run locally, <a href="https://github.com/fugerit-org/fj-doc/tree/main/fj-doc-playground-quarkus">go to the project page</a> or use a public image : </p>
  				</Grid>
  			</Grid>
- 			<Grid id="secondRow" container spacing={1}>
+ 			<Grid id="secondRow" container spacing={1} style={{ paddingLeft: 15 }}>
  	 			<Grid item sx={8}>
  					<TextField sx={{ width: '800px' }} id="outlined-basic" label="docker run" variant="outlined" value={cmdDockerRun}/>
  					<CopyToClipboard text={cmdDockerRun}><Button><ContentCopyIcon /></Button></CopyToClipboard>
  				</Grid>
     		</Grid>
-   			<Grid id="secondRow" container spacing={1}>
+   			<Grid id="secondRow" container spacing={1} style={{ paddingLeft: 15 }}>
  	 			<Grid item sx={8}>
  					And open <a href="http://localhost:8080/fj-doc-playground/home/">http://localhost:8080/fj-doc-playground/home/</a> 
  				</Grid>
