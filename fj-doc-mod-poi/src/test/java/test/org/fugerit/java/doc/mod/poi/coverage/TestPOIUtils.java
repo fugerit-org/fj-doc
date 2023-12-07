7341623f990cf6cf2727670244cb4e07670ff8f8
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-12-07 23:22:27 +0100

    Increased coverage

--- fj-doc-mod-poi/src/test/java/test/org/fugerit/java/doc/mod/poi/coverage/TestPOIUtils.java
+++ fj-doc-mod-poi/src/test/java/test/org/fugerit/java/doc/mod/poi/coverage/TestPOIUtils.java
@@ -5,9 +5,11 @@ import java.io.InputStream;
 import java.util.function.Consumer;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.fugerit.java.core.cfg.ConfigRuntimeException;
 import org.fugerit.java.core.function.SafeFunction;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
+import org.fugerit.java.doc.base.model.DocCell;
 import org.fugerit.java.doc.mod.poi.PoiUtils;
 import org.fugerit.java.doc.mod.poi.WorkbookHelper;
 import org.junit.Assert;
@@ -18,6 +20,41 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TestPOIUtils {
 
+	
+	@Test
+	public void xlsxFormatStyle() {
+		SafeFunction.apply( () -> {
+			boolean ok = true;
+			PoiUtils.xlsxFormatStyle(null, null, null);
+			PoiUtils.xlsxFormatStyle(null, new XSSFCellStyle( null), null );
+			Assert.assertTrue( ok );
+		} );
+	}
+	
+	@Test
+	public void xlsxFontStyle() {
+		SafeFunction.apply( () -> {
+			boolean ok = true;
+			PoiUtils.xlsxFontStyle(null, null, null);
+			Assert.assertTrue( ok );
+		} );
+	}
+	
+	@Test
+	public void lsxFormat() {
+		SafeFunction.apply( () -> {
+			boolean ok = true;
+			try ( InputStream is = ClassHelper.loadFromDefaultClassLoader( "coverage/xls_as_xls.xls" ); 
+					HSSFWorkbook workbook = new HSSFWorkbook( is ) ) {
+				WorkbookHelper helper = new WorkbookHelper(workbook, null);
+				PoiUtils.xlsFormatStyle(helper, null, null);
+				PoiUtils.xlsFormatStyle(helper, workbook.createCellStyle(), null );
+				PoiUtils.xlsFontStyle(helper, null, new DocCell() );
+			}
+			Assert.assertTrue( ok );
+		} );
+	}
+	
 	@Test
 	public void testFindColor() {
 		SafeFunction.apply( () -> {
