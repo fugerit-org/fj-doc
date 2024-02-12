2e831819015245ad50532450f99350b4e68ac1af
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-12 16:47:18 +0100

    Migrate spa to vite #117

---
+++ fj-doc-playground-quarkus/src/main/react/src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Helmet } from 'react-helmet';
+import Playground from './Playground';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const TITLE = 'Venus (fj-doc) playground';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+function App() {
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Helmet>
+                <title>{TITLE}</title>
+            </Helmet>
+            <Playground />
+        </ThemeProvider>
+    );
+}
+
+export default App;
