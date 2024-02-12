0efbf33e0414d05305666a7863cfff2f40d41d69
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-12 01:31:27 +0100

    Link FileIO.isInTmpFolder()

--- fj-doc-playground-quarkus/src/main/java/org/fugerit/java/doc/playground/val/ValUtils.java
+++ fj-doc-playground-quarkus/src/main/java/org/fugerit/java/doc/playground/val/ValUtils.java
@@ -3,6 +3,7 @@ package org.fugerit.java.doc.playground.val;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.fugerit.java.core.function.UnsafeSupplier;
+import org.fugerit.java.core.io.FileIO;
 import org.fugerit.java.core.io.helper.HelperIOException;
 import org.fugerit.java.doc.val.core.DocTypeValidationResult;
 
@@ -15,16 +16,9 @@ import java.util.function.Supplier;
 public class ValUtils {
 
     private ValUtils() {}
-
-    public static boolean isInTmpFolder( File tempFile ) throws IOException {
-        File tempDir = new File( System.getProperty( "java.io.tmpdir" ) );
-        log.info( "file -> {} (tmpdir : {})", tempFile, tempDir );
-        return tempFile.getCanonicalPath().startsWith(tempDir.getCanonicalPath() );
-    }
-
     public static Response doIfInTmpFolder(File tempFile,  UnsafeSupplier<Response, Exception> fun ) throws IOException {
         Response res = null;
-        if ( isInTmpFolder( tempFile ) ) {
+        if ( FileIO.isInTmpFolder( tempFile ) ) {
             res = HelperIOException.get( fun::get );
         } else {
             // no access
