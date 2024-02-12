7df81b6a690a20c07c6bf935d7cd9058eef6e8b7
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-13 00:35:52 +0100

    [playground-quarkus] theme selector

--- fj-doc-playground-quarkus/src/main/react/src/App.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/App.jsx
@@ -4,6 +4,8 @@ import { Helmet } from 'react-helmet';
 import Playground from './Playground';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import {MenuItem, Select} from "@mui/material";
+import React, { useState } from 'react';
 
 const TITLE = 'Venus (fj-doc) playground';
 
@@ -13,15 +15,38 @@ const darkTheme = createTheme({
     },
 });
 
+const lightTheme = createTheme({
+    palette: {
+        mode: 'light',
+    },
+});
+
+const themes = [
+    { key: 'dark', theme: darkTheme },
+    { key: 'light', theme: lightTheme }
+]
+
+
 function App() {
 
+    const [state, setState] = useState({ currentState: darkTheme });
+
     return (
-        <ThemeProvider theme={darkTheme}>
+        <ThemeProvider theme={state.currentState}>
             <CssBaseline />
             <Helmet>
                 <title>{TITLE}</title>
             </Helmet>
             <Playground />
+            <p>Select theme :
+            <Select id="themeCatalog"
+                    onChange={e => {
+                        setState( { currentState: e.target.value } )
+                    }}
+                    value={state.currentState}
+                    defaultValue={state.currentState}>
+                {themes.map( (current) =>  <MenuItem key={current.key} selected={current.theme===state.currentState} value={current.theme}>{current.key}</MenuItem>)}
+            </Select></p>
         </ThemeProvider>
     );
 }
