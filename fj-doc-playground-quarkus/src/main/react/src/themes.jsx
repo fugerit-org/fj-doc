e36b7b1de21be06f62fffd7070b0f713e93330de
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-02-13 21:04:31 +0100

    Added custom theme

--- fj-doc-playground-quarkus/src/main/react/src/themes.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/themes.jsx
@@ -12,7 +12,21 @@ const lightTheme = createTheme({
     },
 });
 
+const customTheme = createTheme( {
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#0f0',
+        },
+        background: {
+            default: '#111111',
+            paper: '#212121',
+        },
+    }
+})
+
 const themes = [
+    { key: 'custom', theme: customTheme },
     { key: 'dark', theme: darkTheme },
     { key: 'light', theme: lightTheme }
 ]
