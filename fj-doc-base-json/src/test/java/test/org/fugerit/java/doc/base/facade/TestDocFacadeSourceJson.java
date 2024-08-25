2366b9a432e220bebf0f6cf454fbbad66f89c58a
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-08-25 17:13:25 +0200

    Assert condition added

--- fj-doc-base-json/src/test/java/test/org/fugerit/java/doc/base/facade/TestDocFacadeSourceJson.java
+++ fj-doc-base-json/src/test/java/test/org/fugerit/java/doc/base/facade/TestDocFacadeSourceJson.java
@@ -1,9 +1,9 @@
 package test.org.fugerit.java.doc.base.facade;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import org.fugerit.java.doc.base.facade.DocFacadeSource;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,37 +12,37 @@ public class TestDocFacadeSourceJson {
 
 	private static final Logger logger = LoggerFactory.getLogger( TestDocFacadeSourceJson.class );
 	
-	private void textSupportedParser( boolean expected, int sourceType ) {
+	private boolean textSupportedParser( boolean expected, int sourceType ) {
 		boolean supported;
 		try {
 			supported = DocFacadeSource.getInstance().isSourceSupported(sourceType);
 			assertEquals( "Failed doc parser supported check", expected , supported );
+			return supported;
 		} catch (Exception e) {
 			String message = "Error : "+e;
 			logger.error( message, e );
-			fail( message );
+			return false;
 		}
-		
 	}
 		
 	@Test
 	public void testParserXml() {
-		this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_XML );
+		Assert.assertTrue( this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_XML ) );
 	}
 	
 	@Test
 	public void textParserDefault() {
-		this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_DEFAULT );
+		Assert.assertTrue( this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_DEFAULT ) );
 	}
 	
 	@Test
 	public void testParserJson() {
-		this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_JSON );
+		Assert.assertTrue( this.textSupportedParser( true, DocFacadeSource.SOURCE_TYPE_JSON ) );
 	}
 	
 	@Test
 	public void testParserYaml() {
-		this.textSupportedParser( false, DocFacadeSource.SOURCE_TYPE_YAML );
+		Assert.assertFalse( this.textSupportedParser( false, DocFacadeSource.SOURCE_TYPE_YAML ) );
 	}
 	
 }
