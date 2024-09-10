358ffc69394b2e86283377f3517b6cd35afc48f3
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-09-10 22:50:03 +0200

    springboot3 template import

---
+++ fj-doc-maven-plugin/src/main/resources/config/flavour/springboot-3/src/main/java/org/fugerit/java/template/fugerit_springboot3_template/FugeritSpringboot3TemplateApplication.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.template.fugerit_springboot3_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FugeritSpringboot3TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FugeritSpringboot3TemplateApplication.class, args);
+	}
+
+}
---
+++ fj-doc-maven-plugin/src/main/resources/config/flavour/springboot-3/src/test/java/org/fugerit/java/template/fugerit_springboot3_template/FugeritSpringboot3TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fugerit.java.template.fugerit_springboot3_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FugeritSpringboot3TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
