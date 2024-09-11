41a8dc27bf68f6289bd610277321902b8fb3750e
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-09-11 17:20:21 +0200

    set next snapshot

---
+++ fj-doc-maven-plugin/src/main/resources/config/flavour/openliberty/src/main/java/org/fugerit/java/template/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package org.fugerit.java.template.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
