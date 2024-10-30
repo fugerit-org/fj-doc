cbb93f897a287a60413ad60045c7f80f493db074
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-10-31 00:54:08 +0100

    [fj-doc-playground-quarkus] review Version as functional component

--- fj-doc-playground-quarkus/src/main/react/src/playground/Version.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/playground/Version.jsx
@@ -1,39 +1,21 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import appService from '../common/app-service';
 
-class Version extends Component {
+const Version = () => {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			versionInfo: null
-		}
-	}
+	const [versionInfo, setVersionInfo] = useState(null)
 
-	componentDidMount() {
-		let reactState = this;
+	useEffect(() => {
 		appService.doAjaxMultipart('GET', '/meta/version', null).then(response => {
 			if (response.success) {
-				reactState.setState({
-					versionInfo: response.result
-				})
-			} else {
-				reactState.setState({
-					supportedExtensions: null
-				})
+				setVersionInfo( response.result )
 			}
 		})
-	}
+	}, []);
 
-	render() {
-		let printVersion = 'loading...';
-		if ( this.state.versionInfo != null ) {
-			printVersion = this.state.versionInfo.version + ' ('+this.state.versionInfo.revision+')';	
-		}
-		return <Fragment>
-			<p>fj-doc-version : {printVersion}</p>
-		</Fragment>
-	}
+	return <Fragment>
+		<p>fj-doc-version : {!versionInfo && 'loading...'} {versionInfo && versionInfo.version &&  (versionInfo.version + ' ('+versionInfo.revision+')')}</p>
+	</Fragment>
 
 }
 
