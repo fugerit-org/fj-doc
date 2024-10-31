e16e9275c98cbfd784a79d5a85ed45b5a74a36f6
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2024-10-31 01:13:05 +0100

    [fj-doc-playground-quarkus] review DocConfigConvert as functional component

--- fj-doc-playground-quarkus/src/main/react/src/playground/DocConfigConvert.jsx
+++ fj-doc-playground-quarkus/src/main/react/src/playground/DocConfigConvert.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { TextField, Grid, Button } from "@mui/material";
 import appService from '../common/app-service';
 
@@ -11,98 +11,66 @@ import "ace-builds/src-noconflict/mode-markdown";
 import "ace-builds/src-noconflict/theme-xcode";
 import "ace-builds/src-noconflict/ext-language_tools";
 
-class DocConfigConvert extends Component {
+const DocConfigConvert = ({handleOpenDialog}) => {
 
-	constructor(props) {
-		super(props);
-		this.handleGenerate = this.handleGenerate.bind(this);
-		this.handleDoc = this.handleDoc.bind(this);
-		this.handleFreemarkerData = this.handleFreemarkerData.bind(this);
-		this.handleEditorContent = this.handleEditorContent.bind(this);
-		this.handleJsonDataContent = this.handleJsonDataContent.bind(this);
-		this.state = {
-			docContent: '',
-			freemarkerJsonData: '',
-			generationTime: null,
-			outputMessage: null,
-			validationEnabled: false
-		}
-	}
+	const [docContent, setDocContent] = useState('')
+	const [docOutput, setDocOutput] = useState('')
+	const [freemarkerJsonData, setFreemarkerJsonData] = useState('')
+	const [outputMessage, setOutputMessage] = useState(null)
 
-	componentDidMount() {
-		this.loadStub();
-	}
+	useEffect(() => {
+		loadStub()
+	}, []);
 
-	loadStub() {
-		let reactState = this;
+	const loadStub = () => {
 		let catalogUrl = '/catalog/res/';
 		appService.doAjaxMultipart('GET', catalogUrl+'convert-config-stub.properties', null).then(response => {
 			if (response.success) {
-				reactState.setState({
-				}, () => reactState.setState( { freemarkerJsonData: response.result } ) )
+				setFreemarkerJsonData( response.result )
 			}
 		})
 		appService.doAjaxMultipart('GET', catalogUrl+'doc-process-stub.xml', null).then(response => {
 			if (response.success) {
-				reactState.setState({
-				}, () => reactState.setState( { docContent: response.result } ) )
+				setDocContent( response.result )
 			}
 		})
 	}
 
-	handleGenerate = (e) => {
+	const handleGenerate = (e) => {
 		e.preventDefault();
-		if (this.state.docContent == null) {
-			this.props.handleOpenDialog("No input to convert");
+		if (docContent == null) {
+			handleOpenDialog("No input to convert");
 		} else {
-			let reactState = this
 			let payload = {}
-			payload.docContent = this.state.docContent;
-			payload.freemarkerJsonData = this.state.freemarkerJsonData;
+			payload.docContent = docContent;
+			payload.freemarkerJsonData = freemarkerJsonData;
 			appService.doAjaxJson('POST', '/config/convert', payload).then(response => {
 				if (response.success) {
-					reactState.setState({
-						docOutput: response.result.docOutputBase64,
-						docFormat: this.state.outputFormat,
-						generationTime:  response.result.generationTime,
-						outputMessage: response.result.message
-					})
+					setDocOutput( response.result.docOutputBase64 )
+					setGenerationTime( response.result.generationTime )
+					setOutputMessage( response.result.message )
 				}
 			})
 		}
 	};
 
-	handleDoc( newValue ) {
-		this.setState({
-			outputFormat: this.state.outputFormat,
-			docContent: newValue
-		});
-	};
+	const handleDoc = ( newValue ) => {
+		setDocContent( newValue )
+	}
 
-	handleFreemarkerData( newValue ) {
-		this.setState({
-			outputFormat: this.state.outputFormat,
-			freemarkerJsonData: newValue
-		});
-	};
+	const handleFreemarkerData = ( newValue ) => {
+		setFreemarkerJsonData( newValue )
+	}
 
-	handleEditorContent = ( content ) => {
-		this.setState(
-			{ 
-				docContent: content			 
-			}
-		);
-	};
+	const handleEditorContent = ( content ) => {
+		setDocContent( content )
+	}
 
-	handleJsonDataContent = ( content ) => {
-		this.setState(
-			{ 
-				freemarkerJsonData: content			 
-			}
-		);
-	};
+	const handleJsonDataContent = ( content ) => {
+		setFreemarkerJsonData( content )
+	}
 
-	 myAtob = (code) => {
+	 const myAtob = (code) => {
 	    try {
 	      return decodeURIComponent(atob(code)
 	      .split('')
@@ -116,10 +84,10 @@ class DocConfigConvert extends Component {
 	    }
 	  }
 
-	render() {
-		let freemarkerJsonData = '';
+	const handleContent = () => {
+		let freemarkerJsonDataSection = '';
 		let editorInFormat = 'xml';
-		freemarkerJsonData = <Fragment>
+		freemarkerJsonDataSection = <Fragment>
 			<p style={{paddingTop: "20px"}}>Configuration conversion parameters, see <a href={'https://github.com/fugerit-org/fj-doc/blob/main/fj-doc-tool/README.md'}>fj-doc-tool</a></p>
 			<AceEditor
 				mode="properties"
@@ -129,16 +97,16 @@ class DocConfigConvert extends Component {
 				enableBasicAutocompletion={true}
 				enableLiveAutocompletion={true}
 				enableSnippets={true}
-				value={this.state.freemarkerJsonData}
-				onChange={this.handleFreemarkerData}
+				value={freemarkerJsonData}
+				onChange={handleFreemarkerData}
 				height='200px'
 				width='100%'
 			/>
 		</Fragment>
 
 		let outputData = <Fragment>Here will be the output</Fragment>
-		if ( this.state.docOutput ) {
-			let decodedStringAtoB = this.myAtob(this.state.docOutput);
+		if ( docOutput ) {
+			let decodedStringAtoB = myAtob(docOutput);
 			outputData = <AceEditor
 				mode='xml'
 				theme="xcode"
@@ -150,7 +118,7 @@ class DocConfigConvert extends Component {
 				value={decodedStringAtoB}
 				width='100%'
 			/>
-		} else if ( this.state.outputMessage != null ) {
+		} else if ( outputMessage != null ) {
 			outputData =  <Grid id="firstRow" container spacing={1} style={{ paddingLeft: 5, paddingTop: 15 }}>
  				<Grid item sx={{ width: '100%' }}>
  					<TextField sx={{ width: '100%' }} 
@@ -158,7 +126,7 @@ class DocConfigConvert extends Component {
  						rows={20}
  						multiline={true}
  						label="output message" 
- 						variant="outlined" value={this.state.outputMessage}/>
+ 						variant="outlined" value={outputMessage}/>
  				</Grid>
  			</Grid>
 		}
@@ -167,7 +135,7 @@ class DocConfigConvert extends Component {
 		    
 			<Grid container spacing={1}>
 			  <Grid item xs={12}>
-			    <Button variant="contained" onClick={this.handleGenerate}>Convert configuration</Button>
+			    <Button variant="contained" onClick={handleGenerate}>Convert configuration</Button>
 			  </Grid>
 			  <Grid item xs={12} xl={6}>
 						<AceEditor
@@ -178,11 +146,11 @@ class DocConfigConvert extends Component {
 							enableBasicAutocompletion={true}
 							enableLiveAutocompletion={true}
 							enableSnippets={true}
-							value={this.state.docContent}
-							onChange={this.handleDoc}
+							value={docContent}
+							onChange={handleDoc}
 							width='100%'
 						/>
-					{freemarkerJsonData}
+					{freemarkerJsonDataSection}
 			  </Grid>
 			  <Grid item xs={12} xl={6}>
 			    {outputData}
@@ -191,6 +159,8 @@ class DocConfigConvert extends Component {
 			
 		</Fragment>
 	}
+
+	return handleContent()
 
 }
 
