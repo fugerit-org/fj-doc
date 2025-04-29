309c7d3aa3f5837994c75625509de900cb400e55
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-04-30 01:31:30 +0200

    [fj-mod-doc-fop] error if a element <phrase> is inside a <cell> (fix) #426

--- fj-doc-mod-fop/src/main/resources/fj_doc_mod_fop_config/template/macro/doc_element.ftl
+++ fj-doc-mod-fop/src/main/resources/fj_doc_mod_fop_config/template/macro/doc_element.ftl
@@ -94,7 +94,7 @@
 						<fo:table-cell<#if (cell.backColor??)> background-color="${cell.backColor}" </#if><@addCssValue name='padding' value=docTable.padding def=0 unit='mm'/><@addCssValue name='margin' value=docTable.padding def=0 unit='mm'/><@handleAlign alignValue=cell.align/><@handleVerticalAlign valignValue=cell.valign/><@handleBorders docBorders=cell.docBorders/><@handleCellSpan cell=cell/>>
 							<#if (cell.elementList?size > 0)>
 								<#list cell.elementList as cellElement>
-								<@handleElement current=cellElement/>
+								<#if cellElement.class.simpleName = 'DocPhrase'><fo:block><@handleElement current=cellElement/></fo:block><o></o><#else><@handleElement current=cellElement/></#if>
 								</#list>
 							<#else>
 								<fo:block></fo:block>
--- fj-doc-sample/src/test/java/test/org/fugerit/java/doc/sample/TestSampleDirect.java
+++ fj-doc-sample/src/test/java/test/org/fugerit/java/doc/sample/TestSampleDirect.java
@@ -1,6 +1,5 @@
 package test.org.fugerit.java.doc.sample;
 
-import org.fugerit.java.core.cfg.ConfigRuntimeException;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
 import org.fugerit.java.doc.lib.direct.VenusDirectFacade;
 import org.fugerit.java.doc.lib.direct.config.VenusDirectConfig;
@@ -18,13 +17,14 @@ class TestSampleDirect {
 
     @Test
     void testDirect() throws IOException {
+        File testOutputMd = new File( "./target/direct-sample/issue-426.md" );
+        testOutputMd.delete();
         try (Reader reader = new InputStreamReader(ClassHelper.loadFromDefaultClassLoader( "venus-config-direct/venus-direct-config.yaml" ) )) {
             Map<String, String> envMap = new HashMap<>();
             envMap.put( "projectBasedir", "." );
             VenusDirectConfig config = VenusDirectFacade.readConfig( reader, envMap );
-            Assertions.assertThrows( ConfigRuntimeException.class, ()-> VenusDirectFacade.handleAllOutput( config ) );
+            VenusDirectFacade.handleAllOutput( config );
         }
-        File testOutputMd = new File( "./target/direct-sample/issue-426.md" );
         Assertions.assertTrue( testOutputMd.exists() );
     }
 
