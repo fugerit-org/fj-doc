95ab0772c21af4aa4b7a78f75620ceee91ecdefb
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-07-06 15:33:06 +0200

    refactor VersionCheck to use fj-core VersionCompare #466

--- fj-doc-maven-plugin/src/test/java/test/org/fugerit/java/doc/project/facade/TestVersionCheck.java
+++ fj-doc-maven-plugin/src/test/java/test/org/fugerit/java/doc/project/facade/TestVersionCheck.java
@@ -18,10 +18,9 @@ class TestVersionCheck {
         Assertions.assertTrue(VersionCheck.isMajorThan( V_8_6_2, "8.6.1-SNAPSHOT" ));
         Assertions.assertFalse(VersionCheck.isMajorThan( "8.5.1", "8.5.2" ));
         VenusContext context = new VenusContext( new File( "target" ), V_8_6_2, "base" );
-        Assertions.assertFalse( context.isPreVersion862() );
         log.info( "version from null to : {}", VersionCheck.findVersion( null ) );
         log.info( "version from null to : {}", VersionCheck.findVersion( VersionCheck.LATEST ) );
-        Assertions.assertEquals( V_8_6_2, VersionCheck.findFromPropsFile( "maven/pom.properties" ) );
+        Assertions.assertFalse( context.isPreVersion862() );
     }
 
 }
