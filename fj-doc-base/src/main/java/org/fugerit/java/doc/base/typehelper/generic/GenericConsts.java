07f6c300067d463fbca8ba3ada8d8c10bfb39e9f
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-08-06 13:52:40 +0200

    doc info property 'table-check-integrity' #480

--- fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/GenericConsts.java
+++ fj-doc-base/src/main/java/org/fugerit/java/doc/base/typehelper/generic/GenericConsts.java
@@ -199,6 +199,10 @@ public class GenericConsts {
 	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#suppress-wrong-type-error">See 'suppress-wrong-type-error' documentation.</a>
 	 */
 	public static final String DOC_SUPPRESS_WRONG_TYPE_ERROR = "suppress-wrong-type-error";
-	
+
+	/**
+	 * <a href="https://venusdocs.fugerit.org/docs/html/doc_meta_info.html#table-check-integrityr">See 'table-check-integrity' documentation.</a>
+	 */
+	public static final String DOC_TABLE_CHECK_INTEGRITY = "table-check-integrity";
 	
 }
