d75211e5b752cf0d40e69426c767a928c61d62fa
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-09-25 00:43:56 +0200

    chore: typo in PdfFopNoAccessibilityTypeHandler  #512

---
+++ fj-doc-mod-fop/src/main/java/org/fugerit/java/doc/mod/fop/PdfFopNoAccessibilityTypeHandler.java
@@ -0,0 +1,9 @@
+package org.fugerit.java.doc.mod.fop;
+
+public class PdfFopNoAccessibilityTypeHandler extends PdfFopTypeHandler {
+
+    public PdfFopNoAccessibilityTypeHandler() {
+        super(  Boolean.FALSE, DEFAULT_KEEP_EMPTY_TAGS );
+    }
+
+}
