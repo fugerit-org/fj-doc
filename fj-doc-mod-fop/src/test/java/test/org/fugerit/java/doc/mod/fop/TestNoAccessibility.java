e1d2785ecde868da9228e3cc4e9ef28ce699e6fe
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-09-24 23:22:00 +0200

    feat: new config properties for PdfFopTypeHandler #508
    "keep-empty-tags" and "accessibility"
    
    This will usually produce smaller PDF.

--- fj-doc-mod-fop/src/test/java/test/org/fugerit/java/doc/mod/fop/TestNoAccessibility.java
+++ fj-doc-mod-fop/src/test/java/test/org/fugerit/java/doc/mod/fop/TestNoAccessibility.java
@@ -3,6 +3,7 @@ package test.org.fugerit.java.doc.mod.fop;
 import lombok.extern.slf4j.Slf4j;
 import org.fugerit.java.core.function.SafeFunction;
 import org.fugerit.java.core.lang.helpers.ClassHelper;
+import org.fugerit.java.core.xml.dom.DOMIO;
 import org.fugerit.java.doc.base.config.DocInput;
 import org.fugerit.java.doc.base.config.DocOutput;
 import org.fugerit.java.doc.base.config.DocTypeHandler;
@@ -13,6 +14,7 @@ import org.fugerit.java.doc.mod.fop.PdfFopTypeNoAccessibilityHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Element;
 import test.org.fugerit.java.BasicTest;
 
 import java.io.FileOutputStream;
@@ -21,10 +23,10 @@ import java.io.InputStreamReader;
 @Slf4j
 class TestNoAccessibility extends BasicTest {
 
-	private boolean testHelper( DocTypeHandler handler ) {
+	private boolean testHelper( String testCase, DocTypeHandler handler ) {
 		boolean ok = false;
 		try ( InputStreamReader reader = new InputStreamReader( ClassHelper.loadFromDefaultClassLoader( "sample/doc_alt_01.xml" ) );
-				FileOutputStream fos = new FileOutputStream( "target/test_no_accessibilit-"+handler.getClass().getSimpleName()+"."+handler.getType() ) ) {
+				FileOutputStream fos = new FileOutputStream( "target/test_no_accessibilit-"+testCase+"."+handler.getType() ) ) {
 			handler.handle( DocInput.newInput( handler.getType(), reader ) , DocOutput.newOutput( fos ) );
 			ok = true;
 			log.info( "result {}", ok );
@@ -37,11 +39,21 @@ class TestNoAccessibility extends BasicTest {
 	private static final DocTypeHandler[] HANDLERS = { PdfFopTypeHandler.HANDLER, new PdfFopTypeNoAccessibilityHandler() };
 	
 	@Test
-	void testNoAccessibility1() {
+	void testNoAccessibilityHandler() {
 		for ( int k=0; k<HANDLERS.length; k++ ) {
-			boolean ok = this.testHelper(HANDLERS[k]);
+			boolean ok = this.testHelper( HANDLERS[k].getClass().getSimpleName(), HANDLERS[k]);
 			Assertions.assertTrue(ok);
 		}
+	}
+
+	@Test
+	void testNoAccessibilityConfig() throws Exception {
+		PdfFopTypeHandler handler = new PdfFopTypeHandler();
+		this.testHelper( "test-config-pre", handler );
+		Element config = DOMIO.loadDOMDoc( "<conf><docHandlerCustomConfig accessibility='false' keep-empty-tags='true'/></conf>" ).getDocumentElement();
+		handler.configure( config );
+		this.testHelper( "test-config-post", handler );
+
 	}
 	
 }
