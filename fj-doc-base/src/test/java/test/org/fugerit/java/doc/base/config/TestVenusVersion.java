713cdbe9a1a5a9f0425047ad962762f524bfe91b
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-10-02 22:43:31 +0200

    feat: coverage #523

--- fj-doc-base/src/test/java/test/org/fugerit/java/doc/base/config/TestVenusVersion.java
+++ fj-doc-base/src/test/java/test/org/fugerit/java/doc/base/config/TestVenusVersion.java
@@ -2,9 +2,11 @@ package test.org.fugerit.java.doc.base.config;
 
 import org.fugerit.java.doc.base.config.VenusVersion;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class TestVenusVersion {
 
+    @Test
     void testVersion() {
         Assertions.assertNotNull( VenusVersion.getFjDocCoreVersionS() );
         Assertions.assertNotNull( VenusVersion.getFjDocCoreVersionS() );
