87646e76b06d21ab53a7c0805eacdb030cef1538
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-10-02 10:21:00 +0200

    chore: sonar issue removal

--- fj-doc-base-yaml/src/main/java/org/fugerit/java/doc/yaml/parse/DocYamlToXml.java
+++ fj-doc-base-yaml/src/main/java/org/fugerit/java/doc/yaml/parse/DocYamlToXml.java
@@ -3,7 +3,6 @@ package org.fugerit.java.doc.yaml.parse;
 import org.fugerit.java.doc.json.parse.DocJsonToXml;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 public class DocYamlToXml extends DocJsonToXml {
 
