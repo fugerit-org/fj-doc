cdd047c90b4d699737eca6aa21e3c7fb20c28ed4
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2026-04-02 23:38:24 +0200

    chore: added integration test

---
+++ fj-doc-playground-quarkus/src/test/java/org/fugerit/java/doc/playground/CatalogRestIt.java
@@ -0,0 +1,8 @@
+package org.fugerit.java.doc.playground;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class CatalogRestIt extends CatalogRestTest {
+
+}
---
+++ fj-doc-playground-quarkus/src/test/java/org/fugerit/java/doc/playground/GenerateRestIt.java
@@ -0,0 +1,10 @@
+package org.fugerit.java.doc.playground;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusIntegrationTest
+class GenerateRestIt extends GenerateRestTest {
+
+}
